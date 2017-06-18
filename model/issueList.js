8dd93502b45dd799bbb96b55bd6a0d7f954685ad
sharuk ahmed mazumder <sharuk.ahmed@coda.global>
2017-06-18 08:12:54 +0530

    error messages

--- model/issueList.js
+++ model/issueList.js
@@ -17,18 +17,39 @@ exports.getAllIssue = function (data, callBack) {
     if(data.toDate !== ''){
         options.path+='%20and%20updated<'+data.toDate;
     }
+    if(!options.host.endsWith('.atlassian.net')){
+         callBack({ 'users': [], isValid :false, errorHeader:'Incorrect jira domain' ,errorMessage: 'Check if your jira domain is valid'});
+         return;
+    }
     https.request(options, function (httpResponse) {
         //another chunk of data has been recieved, so append it to `str`
-        if(httpResponse.statusCode === 401)
+        if(httpResponse.statusCode === 401 || httpResponse.statusCode === 400 || httpResponse.statusCode === 502)
         {
-            callBack({ 'users': [], isValid :false });
+            let errorHeader = '';
+            let errorMessage = ''
+                switch(httpResponse.statusCode) {
+                case 401:
+                    errorHeader = 'Login failed';
+                    errorMessage = 'Invalid username or password.';
+                    break;
+                case 400:
+                    errorHeader = 'Incorrect project';
+                    errorMessage = 'Please check your project name.';
+                    break;
+                case 502:
+                    errorHeader = 'Incorrect jira domain';
+                    errorMessage = 'Ensure it is without http:// and has no trailing /';
+                    break;
+            }
+            callBack({ 'users': [], isValid :false, errorHeader:errorHeader , errorMessage:errorMessage});
             return;
         }
         httpResponse.on('data', function (chunk) {
             str += chunk;
         });
-        //the whole response has been recieved, so we just print it out here
+
         httpResponse.on('end', function () {
+            
             let currentThis = this;
             let allIssue = issueHelper.getAllIssuesHelper(str);
             let moreThan20log = allIssue.moreThan20Logs;
@@ -50,7 +71,7 @@ exports.getAllIssue = function (data, callBack) {
             }
             if (moreThan20log.length === 0) {
                 let users = issueHelper.mapUserAndIssue(allIssue.lessThan21Logs);
-                callBack({ 'users': users, isValid :true });
+                callBack({ 'users': users, isValid :true , isBadRequest:false});
             }
         });
     }).end();
